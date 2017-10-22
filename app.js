b1ceb700f34f2dbbe8e40cd05a3d44ff6855c3ed
Ritagya <mritagya@gmail.com>
2017-10-22 16:14:56 -0700

    Minor change for total score

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ document.querySelector('.btn-hold').addEventListener('click' , function(){
 	document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
 	
 	// 3. Check if player won the game
-	if(scores[activePlayer] >= 10){
+	if(scores[activePlayer] >= 100){
 		document.querySelector('#name-' + activePlayer).textContent = "Winner!"
 		document.querySelector('.dice').style.display = 'none';
 		
